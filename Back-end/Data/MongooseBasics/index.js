96e6ac92284be8c8659c8e2aee9770efa6b6adb5
Brendan Thompson <65555644+DavidBThomp@users.noreply.github.com>
2022-11-30 11:28:42 -0600

    Mongoose Migration + Server Connected

--- Back-end/Data/MongooseBasics/index.js
+++ Back-end/Data/MongooseBasics/index.js
@@ -1,12 +1,15 @@
+// Mongooes Update: https://github.com/sidmirza4/YelpCamp-v2/blob/mongoose-fix/README.md
+
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!!!!")
-        console.log(err)
-    })
+
+main().catch(err => console.log('Error Connecting to Database.', err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+
+    // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+  }
 
 const movieSchema = new mongoose.Schema({
     title: String,
@@ -16,20 +19,21 @@ const movieSchema = new mongoose.Schema({
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
-// const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
 
 
-// Movie.insertMany([
-//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
-//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
-//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
-//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
-//     { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
-// ])
-//     .then(data => {
-//         console.log("IT WORKED!")
-//         console.log(data);
-//     })
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+])
+    .then(data => {
+        console.log("IT WORKED!")
+        console.log(data);
+    })
 
 
 
